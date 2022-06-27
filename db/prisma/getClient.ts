9cc26d37c763bca27bc92457bd7318bde39a6907
nickadamson <naburner@protonmail.com>
2022-06-27 15:28:03 -0700

    add prisma & setup config

---
+++ db/prisma/getClient.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from "@prisma/client";
+
+// NodeJS.Global namespace was removed in @types/node@16.0.0
+declare global {
+  namespace NodeJS {
+    interface Global {}
+    interface InspectOptions {}
+    interface ConsoleConstructor extends console.ConsoleConstructor {}
+  }
+}
+
+// add prisma to the NodeJS global type
+interface CustomNodeJsGlobal extends NodeJS.Global {
+  prisma: PrismaClient;
+}
+
+// Prevent multiple instances of Prisma Client in development
+declare const global: CustomNodeJsGlobal;
+
+const getClient = () => {
+  //   return new PrismaClient({ errorFormat: "pretty", log: ["query", "info"] });
+  const Prisma =
+    global.prisma ||
+    new PrismaClient({
+      errorFormat: "pretty",
+      log: ["query", "info", "warn", "error"],
+    });
+
+  if (process.env.NODE_ENV === "development") {
+    global.prisma = Prisma;
+  }
+
+  return Prisma;
+};
+
+export default getClient;
