7924750c5a175358b5126245afee4a655e10412f
nickadamson <naburner@protonmail.com>
2022-06-27 16:06:23 -0700

    test api connection

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import getClient from "@/prisma/getClient";
 
 type Data = {
-  name: string
-}
+  provider: string;
+};
 
-export default function handler(
+type Err = {
+  err: string;
+};
+
+const prisma = getClient();
+
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data | Err>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  try {
+    const provider = (prisma as any)?._activeProvider;
+    console.log(`Prisma successfully connected to ${provider} database.`);
+    res.status(200).json({ provider: provider });
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+    res.status(500).json({ err: `Internal Server Error` });
+  }
 }
